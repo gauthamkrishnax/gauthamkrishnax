4345633675ba07699ba6538ee08f51e022eddbba
gauthamksn <gsnair@adobe.com>
2026-04-24 10:41:33 +0530

    small changes + scroll

--- src/pages/+Layout.tsx
+++ src/pages/+Layout.tsx
@@ -5,11 +5,13 @@ import { ReactLenis } from 'lenis/react'
 import 'lenis/dist/lenis.css'
 import React from 'react'
 import Footer from '../components/Footer'
+import RouteScrollReset from '../components/RouteScrollReset'
 
 function Layout({ children }: { children: React.ReactNode }) {
     return (
         <React.StrictMode>
-            <ReactLenis root>
+            <ReactLenis root options={{ stopInertiaOnNavigate: true }}>
+                <RouteScrollReset />
                 {children}
                 <Footer />
             </ReactLenis>
